796cad723d7443be37f7315fbd63cae729267a47
suwei-PC <su_wei1989@qq.com>
2018-11-23 16:54:04 +0800

    巧匠手绘专题20180522

---
+++ shouhui-20180522/js/ztpage.js
@@ -0,0 +1,40 @@
+$(function() {
+    if($('#sld-zt').size()) {
+        $('#sld-zt').slides({
+            generatePagination: true,
+            generateNextPrev: true,
+            play: 3000,
+            pause: 2500,
+            hoverPause: true,
+            animationComplete: function(i) {
+                $('.tab-xyzp > li').eq(i - 1).addClass('cur').siblings().removeClass('cur');
+            }
+        });
+    }
+    $('.tab-xyzp > li').click(function() {
+        $(this).addClass('cur').siblings().removeClass('cur');
+        $('#sld-zt .pagination > li').eq($(this).index()).find('a').click();
+    });
+
+    // 点击小图显示大图
+    jQuery_New("a[rel^='gallery[modal]']").prettyPhoto({theme: 'duotive-modal', opacity:0.5, show_title: false, overlay_gallery:true});
+    jQuery_New("a[rel^='gallery2[modal]']").prettyPhoto({theme: 'duotive-modal', opacity:0.5, show_title: false, overlay_gallery:true});
+    jQuery_New("a[rel^='gallery3[modal]']").prettyPhoto({theme: 'duotive-modal', opacity:0.5, show_title: false, overlay_gallery:true});
+    jQuery_New("a[rel^='gallery4[modal]']").prettyPhoto({theme: 'duotive-modal', opacity:0.5, show_title: false, overlay_gallery:true});
+    jQuery_New("a[rel^='gallery5[modal]']").prettyPhoto({theme: 'duotive-modal', opacity:0.5, show_title: false, overlay_gallery:true});
+
+
+    showSideBar();
+    $(window).on('scroll', function() {
+        showSideBar();
+    });
+});
+
+function showSideBar() {
+    if($(window).scrollTop() > $(window).height()) {
+        $('.btn-talk').show();
+    }
+    else {
+        $('.btn-talk').hide();
+    }
+}
