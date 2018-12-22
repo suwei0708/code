8b0ebf754158ac4470f79876fdd7d3100eec994d
suwei-MPC <304900839@qq.com>
2018-12-22 22:25:31 +0800

    逆袭专题

--- nixi20181221/js/page-zt.js
+++ nixi20181221/js/page-zt.js
@@ -5,10 +5,7 @@ $(function() {
             generateNextPrev: true,
             play: 3000,
             pause: 2500,
-            hoverPause: true,
-            // animationComplete: function(i) {
-            //     $('.tab-xyzp > li').eq(i - 1).addClass('cur').siblings().removeClass('cur');
-            // }
+            hoverPause: true
         });
     }
     if($('#sld-zt2').length) {
@@ -20,10 +17,32 @@ $(function() {
             hoverPause: true
         });
     }
+    if($('#sld-zt3').length) {
+        $('#sld-zt3').slides({
+            generatePagination: true,
+            generateNextPrev: true,
+            play: 3000,
+            pause: 2500,
+            hoverPause: true,
+            animationComplete: function(i) {
+                $('.tab-xyzp > li').eq(i - 1).addClass('cur').siblings().removeClass('cur');
+            }
+        });
+    }
     $('.tab-xyzp > li').click(function() {
         $(this).addClass('cur').siblings().removeClass('cur');
-        $('#sld-zt .pagination > li').eq($(this).index()).find('a').click();
+        $('#sld-zt3 .pagination > li').eq($(this).index()).find('a').click();
     });
+
+    if($('#sld-zt4').length) {
+        $('#sld-zt4').slides({
+            generatePagination: true,
+            generateNextPrev: false,
+            play: 3000,
+            pause: 2500,
+            hoverPause: true
+        });
+    }
 
     // 点击小图显示大图
     jQuery_New("a[rel^='gallery[modal]']").prettyPhoto({theme: 'duotive-modal', opacity:0.5, show_title: false, overlay_gallery:true});
