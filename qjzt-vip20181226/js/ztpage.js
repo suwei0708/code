c1833cc6c0f22be3917c62ae084e39128410f4d7
suwei-MPC <304900839@qq.com>
2018-12-27 00:46:33 +0800

    vip专题

--- qjzt-vip20181226/js/ztpage.js
+++ qjzt-vip20181226/js/ztpage.js
@@ -20,6 +20,32 @@ $(function() {
             date: downcount
         });
     };
+
+    // 讲师轮播
+    if ($('.silder-box-zt').length) {
+        //回调函数计数
+        var callbackIndex = 0;
+        $('.silder-box-zt').mySilder({
+            width: 200, //容器的宽度 必选参数!!!!!!
+            height: 638, //容器的高度 必选参数!!!!!!
+            auto: true, //是否自动滚动
+            autoTime: 5, //自动滚动时，时间间隙，即多长滚动一次,单位(秒)
+            direction: 'x', //滚动方向,默认X方向
+            autoType: 'left', //滚动方向，auto为true时生效
+            few: 1, //一次滚动几个，默认滚动1张
+            showFew: 5, //显示几个,就不用调css了,默认显示一个
+            clearance: 0, //容器之间的间隙，默认为 0
+            silderMode: 'linear', //滚动方式
+            timeGap: 650, //动画间隙，完成一次动画需要多长时间，默认700ms
+            buttonPre: '.left1', //上一个，按钮
+            buttonNext: '.right1', //下一个，按钮
+            jz: true, //点击时，是否等待动画完成
+            runEnd: function() { //回调函数
+                // callbackIndex++;
+                // $('.cj em').text(callbackIndex);
+            }
+        });
+    }
 });
 
 // 倒计时插件
