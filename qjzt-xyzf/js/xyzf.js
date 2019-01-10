fcb3f99f447162393e130491ab54e394e8417c70
suwei-PC <su_wei1989@qq.com>
2019-01-10 17:22:08 +0800

    学员专访

--- qjzt-xyzf/js/xyzf.js
+++ qjzt-xyzf/js/xyzf.js
@@ -16,8 +16,39 @@ $(function() {
             play: 3000,
             pause: 2500,
             hoverPause: true,
-            next: 'qj-fanyezuobianjiantou',
-            prev: 'qj-fanyeyoubianjiantou'
+            next: 'izf-fanyer',
+            prev: 'izf-fanyel'
+        });
+    }
+
+    if($('#xyzf-sld2').length) {
+        $('#xyzf-sld2').slides({
+            generatePagination: true,
+            generateNextPrev: true,
+            play: 3000,
+            pause: 2500,
+            hoverPause: true,
+            next: 'izf-fanyer',
+            prev: 'izf-fanyel',
+            animationComplete: function(i) {
+                $('.tab-xyzp > li').eq(i - 1).addClass('cur').siblings().removeClass('cur');
+            }
+        });
+        $('.tab-xyzp > li').on('click', function() {
+            $(this).addClass('cur').siblings().removeClass('cur');
+            $('#xyzf-sld2 .pagination > li').eq($(this).index()).find('a').click();
+        });
+    }
+
+    if($('#xyzf-sld3').length) {
+        $('#xyzf-sld3').slides({
+            generatePagination: true,
+            generateNextPrev: true,
+            play: 3000,
+            pause: 2500,
+            hoverPause: true,
+            next: 'izf-fanyer',
+            prev: 'izf-fanyel'
         });
     }
 });
