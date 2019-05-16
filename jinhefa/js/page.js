eb27da1a6ba547a8a3a2fb58763566859cceda60
suwei-MPC <304900839@qq.com>
2019-05-16 23:31:15 +0800

    金合发

--- jinhefa/js/page.js
+++ jinhefa/js/page.js
@@ -1,33 +1,32 @@
 $(function() {
     // 首页轮播
     if($('.swiper1').find('.swiper-slide').length > 1) {
-        new Swiper('.swiper1',{
-            autoplay: 5000,//可选选项，自动滑动
-            pagination : '.pagination1',
-            loop: true,
-        });
+        var mySwiper = new Swiper('.swiper1',{
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+            },
+            loop : true
+        })
     };
 
-    // 复制
-    $('.func').on('click', '.btn-copy', function() {
-        if(!$('#contents').length) {
-            $('body').append('<div style="width: 0; height: 0; position: absolute; z-index: -999;"><textarea style="width: 0; height: 0;" id="contents"></textarea></div>')
+    // 下拉
+    $('.header-menu').on('click', function(e) {
+        if ($(this).find('ul').is(':visible')) {
+            $(this).find('ul').slideUp();
         }
-        $('#contents').val($(this).data('text'));
-        copyToClipboard();
-        alert('复制成功！')
+        else {
+            $(this).find('ul').slideDown();
+        }
+        e.stopPropagation()
     });
-
-    // 内容页tab切换
-    $('.details-nav').on('click', 'a', function() {
-        $(this).addClass('cur').siblings().removeClass('cur');
-        $('.tab-cont').hide().eq($(this).index()).show();
-    });
+    $('body').on('click', function() {
+        if ($('.header-menu').find('ul').is(':visible')) {
+            $('.header-menu').find('ul').slideUp();
+        }
+    })
 });
-
-/*复制代码到剪切板*/
-function copyToClipboard(){
-    var e = document.getElementById("contents");//对象是contents
-    e.select(); //选择对象
-    document.execCommand("Copy"); //执行浏览器复制命令
-}
