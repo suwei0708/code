ad79e2ede3e0c3239513b61e15a284a9ff73bd69
suwei-MPC <304900839@qq.com>
2019-09-04 02:40:18 +0800

    2019金合发pc

--- 2019金合發PC/js/page.js
+++ 2019金合發PC/js/page.js
@@ -28,5 +28,63 @@ $(function() {
             hoverPause: true,
 		});
     };
+    // 首页banner
+    if ($('#sld2').length) {
+        $('#sld2').slides({
+            generatePagination: true,
+            generateNextPrev: false,
+            play: 3000,
+            pause: 2500,
+            hoverPause: true,
+        });
+    };
 
+	if ($('.slider-box1').length) {
+	    //回调函数计数
+	    var callbackIndex = 0;
+	    $('.slider-box1').mySilder({
+	        width: 190, //容器的宽度 必选参数!!!!!!
+	        height: 180, //容器的高度 必选参数!!!!!!
+	        auto: true, //是否自动滚动
+	        autoTime: 5, //自动滚动时，时间间隙，即多长滚动一次,单位(秒)
+	        direction: 'x', //滚动方向,默认X方向
+	        autoType: 'left', //滚动方向，auto为true时生效
+	        few: 1, //一次滚动几个，默认滚动1张
+	        showFew: 4, //显示几个,就不用调css了,默认显示一个
+	        clearance: 10, //容器之间的间隙，默认为 0
+	        silderMode: 'linear', //滚动方式
+	        timeGap: 500, //动画间隙，完成一次动画需要多长时间，默认700ms
+	        buttonPre: '.btl', //上一个，按钮
+	        buttonNext: '.play-next1', //下一个，按钮
+	        jz: true, //点击时，是否等待动画完成
+	        runEnd: function() { //回调函数
+	            callbackIndex++;
+	            // $('.cj em').text(callbackIndex);
+	        }
+	    });
+	}
+	if ($('.slider-box2').length) {
+	    //回调函数计数
+	    var callbackIndex2 = 0;
+	    $('.slider-box2').mySilder({
+	        width: 190, //容器的宽度 必选参数!!!!!!
+	        height: 180, //容器的高度 必选参数!!!!!!
+	        auto: true, //是否自动滚动
+	        autoTime: 5, //自动滚动时，时间间隙，即多长滚动一次,单位(秒)
+	        direction: 'x', //滚动方向,默认X方向
+	        autoType: 'left', //滚动方向，auto为true时生效
+	        few: 1, //一次滚动几个，默认滚动1张
+	        showFew: 4, //显示几个,就不用调css了,默认显示一个
+	        clearance: 10, //容器之间的间隙，默认为 0
+	        silderMode: 'linear', //滚动方式
+	        timeGap: 500, //动画间隙，完成一次动画需要多长时间，默认700ms
+	        buttonPre: '.btl2', //上一个，按钮
+	        buttonNext: '.play-next2', //下一个，按钮
+	        jz: true, //点击时，是否等待动画完成
+	        runEnd: function() { //回调函数
+	            callbackIndex2++;
+	            // $('.cj em').text(callbackIndex);
+	        }
+	    });
+	}
 });
