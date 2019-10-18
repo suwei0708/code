eff2c45608b12ff5e3ee6a18a4dc40dc13d297d3
suwei-PC <304900839@qq.com>
2019-10-18 16:58:42 +0800

    致设计康佳专题

--- zsj-konka/js/ztpage.js
+++ zsj-konka/js/ztpage.js
@@ -1,61 +1,18 @@
 $(function() {
-	// 导航跳转
-	if ($('#scroll0').length) {
-		showCur();
-		var timer;
-	    $(window).on('scroll', function() {
-			clearInterval(timer);
-			timer = setTimeout(showCur, 100);
-	    });
-		$('.zt-nav').on('click', 'li', function() {
-			var index = +$(this).index();
-			if (index == 0) {
-				$('html, body').animate({
-					scrollTop: $('.zt-banner').offset().top
-				}, 300);
-			}
-			else {
-				$('html, body').animate({
-					scrollTop: $('#scroll' + index).offset().top - $('.zt-nav').height()
-				}, 300);
-			}
+	// 导航滑动
+	if($('#dspw').length) {
+		fixedObj('.nav-zt', '.zt-banner', 86);
+		$('#nav-zt').on('click', 'a', function() {
+		    if ($(this).attr('href').indexOf('html#') > -1) {
+		        console.log('aaa')
+		        if ($($(this).attr('href')).length) {
+		            $('html, body').animate({
+		                'scrollTop': $($(this).attr('href')).offset().top
+		            }, 500);
+		            return false;
+		        }
+		    }
 		});
-	};
-
-	function showCur() {
-		// 导航显示
-		if ($(window).scrollTop() > $('.zt-banner').offset().top + $('.zt-banner').height()) {
-			$('.zt-nav').css({
-				'position': 'fixed'
-			});
-			$('.zt-slogan').css({
-				'margin-top': $('.zt-nav').outerHeight()
-			});
-		}
-		else {
-			$('.zt-nav').css({
-				'position': 'static'
-			});
-			$('.zt-slogan').css({
-				'margin-top': 0
-			});
-		};
-
-		$.each($('.zt-nav li'), function(i) {
-			if (i == 0) {
-				$('.zt-nav li').removeClass('cur').eq(0).addClass('cur');
-			}
-			else if ($(window).scrollTop() >= $('#scroll' + i).offset().top - $('.zt-nav').height()) {
-				$('.zt-nav li').removeClass('cur').eq(i).addClass('cur');
-			}
-		});
-
-		if ($(window).scrollTop() > $(window).height()) {
-		    $('.zt-talk').show();
-		}
-		else {
-		    $('.zt-talk').hide();
-		};
 	}
 
 	// 无缝滚动
@@ -69,3 +26,36 @@ $(function() {
 	    });
 	};
 });
+
+function fixedObj(box, obj, distance) {
+    var position = $(box).offset().top;
+    judgeFix(box, obj, position, distance);
+
+    $(window).on('scroll', function() {
+        judgeFix(box, obj, position, distance);
+    });
+}
+
+function judgeFix(box, obj, position, distance) {
+    if (distance) {
+        num = distance
+    } else {
+        num = 0
+    }
+    if ($(window).scrollTop() > position) {
+        $(box).addClass('pos-fixed');
+        if (obj) {
+            $(obj).css({
+                'margin-bottom': num
+            });
+        }
+    } else {
+        $(box).removeClass('pos-fixed');
+        if (obj) {
+            $(obj).css({
+                'margin-bottom': 0
+            });
+        }
+    }
+
+}
