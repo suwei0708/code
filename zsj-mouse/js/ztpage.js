ba19e43821ec7ec5fbe83d0a138fc995e89634cb
suwei-PC <304900839@qq.com>
2019-11-14 17:33:13 +0800

    致设计鼠灵感

--- zsj-mouse/js/ztpage.js
+++ zsj-mouse/js/ztpage.js
@@ -13,8 +13,29 @@ $(function() {
 	        $('.zt-talk').show();
 	    } else {
 	        $('.zt-talk').hide();
-	    };
+		};
+
+		if ($('.zt-nav a[data-id]').length) {
+			if ($(window).scrollTop() >= $('#' + $('.zt-nav a[data-id]').attr('data-id')).offset().top) {
+				$('.zt-nav a').removeClass('cur');
+				$('.zt-nav a[data-id]').addClass('cur');
+			}
+			else {
+				$('.zt-nav a').removeClass('cur');
+				$('.zt-nav a:eq(0)').addClass('cur');
+			}
+		}
 	}
+
+	// 导航滑动
+	$('.zt-nav').on('click', 'a', function() {
+		if ($(this).attr('data-id').length) {
+			$('html, body').animate({
+				'scrollTop': $('#' + $(this).attr('data-id')).offset().top
+			}, 500);
+			return false;
+		}
+	});
 });
 
 function fixedObj(box, obj, distance) {
